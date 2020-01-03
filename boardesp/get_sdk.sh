a4b343e3801825ea05aa6fd67a424c231b8939dd
Martin Zielinski <darthcoder1@gmail.com>
2020-01-03 17:52:27 +0100

    fix some issues with get_sdk.sh for docker setup

--- boardesp/get_sdk.sh
+++ boardesp/get_sdk.sh
@@ -1,14 +1,22 @@
+
+
+SUDO=sudo
+if [ -z "$1" ]; then
+    SUDO=
+fi
+
+
 #!/bin/bash
-apt-get install -y make unrar-free autoconf automake libtool gcc g++ gperf \
+$SUDO apt-get install -y make unrar-free autoconf automake libtool gcc g++ gperf \
     flex bison texinfo gawk ncurses-dev libexpat-dev python-dev python python-serial \
     sed git unzip bash help2man wget bzip2
 
 # huh?
-apt-get install -y libtool
-apt-get install -y libtool-bin
+$SUDO apt-get install -y libtool
+$SUDO apt-get install -y libtool-bin
 git clone --recursive https://github.com/pfalcon/esp-open-sdk.git
 cd esp-open-sdk
 #git checkout 03f5e898a059451ec5f3de30e7feff30455f7cec
 #cp ../python2_make.py .
 #python2 python2_make.py 'LD_LIBRARY_PATH="" make STANDALONE=y'
-make
+CT_ALLOW_BUILD_AS_ROOT=y make
