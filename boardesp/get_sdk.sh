401bd5ce5c166086f378e8f3b6d7a34797142534
Martin Zielinski <darthcoder1@gmail.com>
2020-01-03 23:57:20 +0100

    fix arg passing to make

--- boardesp/get_sdk.sh
+++ boardesp/get_sdk.sh
@@ -11,12 +11,8 @@ $SUDO apt-get install -y make unrar-free autoconf automake libtool gcc g++ gperf
     flex bison texinfo gawk ncurses-dev libexpat-dev python-dev python python-serial \
     sed git unzip bash help2man wget bzip2
 
-# huh?
 $SUDO apt-get install -y libtool
 $SUDO apt-get install -y libtool-bin
 git clone --recursive https://github.com/pfalcon/esp-open-sdk.git
 cd esp-open-sdk
-#git checkout 03f5e898a059451ec5f3de30e7feff30455f7cec
-#cp ../python2_make.py .
-#python2 python2_make.py 'LD_LIBRARY_PATH="" make STANDALONE=y'
-CT_ALLOW_BUILD_AS_ROOT=y make
+make CT_ALLOW_BUILD_AS_ROOT=y
